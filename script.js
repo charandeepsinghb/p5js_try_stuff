d724fc55ae4587685dfbf122f1badcdcbfb41030
Charandeep Singh <charandeeps81@gmail.com>
2023-03-01 20:39:36 +0530

    simple x square function

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function setup() {
     createCanvas(400, 400);
 }
-
+let x = 0;
 function draw() {
-    background(220);
-    frameRate(2);
+    // init part
+    // background(220);
+    frameRate(10);
     line(20, 380, 380, 380);
     line(20, 20, 20, 380);
 
+    point(x * 8 + 200, 360 + -x*x);
+
+    x++;
     
+    // console.log(x);
 }
