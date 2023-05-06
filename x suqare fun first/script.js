d299e0cbd3d2a61d713e40fc447f28bbedf541be
Charandeep Singh <charandeeps81@gmail.com>
2023-05-06 21:24:34 +0530

    Updatd code according to X square function

---
+++ x suqare fun first/script.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 400);
+}
+let x = 0;
+function draw() {
+    // init part
+    // background(220);
+    frameRate(10);
+    line(20, 380, 380, 380);
+    line(20, 20, 20, 380);
+
+    point(x * 8 + 200, 360 + -x*x);
+
+    x += 0.2;
+    
+    // console.log(x);
+}
